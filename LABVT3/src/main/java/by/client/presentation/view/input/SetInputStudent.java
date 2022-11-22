2640420d3d08839a7af4682d200e4ac401376a87
Yglv <78694320+Yglv@users.noreply.github.com>
2022-11-22 10:53:40 +0300

    Delete LABVT3 directory

--- LABVT3/src/main/java/by/client/presentation/view/input/SetInputStudent.java
+++
@@ -1,7 +0,0 @@
-package main.java.by.client.presentation.view.input;
-
-import main.java.by.client.entity.Student;
-
-public interface SetInputStudent {
-    boolean setInput(Student student, String input);
-}
--- LABVT3/src/main/java/by/client/presentation/view/input/SetInputUser.java
+++
@@ -1,7 +0,0 @@
-package main.java.by.client.presentation.view.input;
-
-import main.java.by.client.entity.user.User;
-
-public interface SetInputUser {
-    boolean setInput(User user, String input);
-}
